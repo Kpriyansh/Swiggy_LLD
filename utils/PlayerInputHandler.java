7c3142320f384e501a7cf4fceafca20cf8249228
Priyansh Kumar <71462800+Kpriyansh@users.noreply.github.com>
2024-05-04 15:42:08 +0530

    Correcting PlayerInputHandler

--- utils/PlayerInputHandler.java
+++ utils/PlayerInputHandler.java
@@ -15,7 +15,7 @@ public class PlayerInputHandler {
     }
 
     private static void inputPlayerName(Player P) {
-        System.out.println("Input name of the Player: ");
+        System.out.print("\nInput name of the Player: ");
         String name = scanner.next();
         if (name.isEmpty()) {
             // throw exception
@@ -25,7 +25,7 @@ public class PlayerInputHandler {
     }
 
     private static void inputPlayerStrength(Player P) {
-        System.out.println("Input strength of the Player: ");
+        System.out.print("\nInput strength of the Player: ");
         int strength = scanner.nextInt();
         if (!validAttribute(strength)) {
             // throw exception
@@ -35,28 +35,29 @@ public class PlayerInputHandler {
     }
 
     private static void inputPlayerAttack(Player P) {
-        System.out.println("Input attack of the Player: ");
+        System.out.print("\nInput attack of the Player: ");
         int attack = scanner.nextInt();
         if (!validAttribute(attack)) {
             // throw exception
             throw new IllegalArgumentException("Attack must be a Positive Integer");
 
         }
-        P.setStrength(Integer.valueOf(attack));
+        P.setAttack(Integer.valueOf(attack));
     }
 
     private static void inputPlayerHealth(Player P) {
-        System.out.println("Input health of the Player: ");
+        System.out.print("\nInput health of the Player: ");
         int health = scanner.nextInt();
         if (!validAttribute(health)) {
             // throw exception
             throw new IllegalArgumentException("Health must be a Positive Integer");
 
         }
-        P.setStrength(Integer.valueOf(health));
+        P.setHealth(Integer.valueOf(health));
     }
 
     private static boolean validAttribute(int attr) {
         return attr > 0;
     }
+
 }
